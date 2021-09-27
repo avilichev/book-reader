2fcbccf105ce39e160db05f21b3f6ea90aae7115
Ильичев Александр Валерьевич <alexander.ilichev@russianpost.ru>
2021-09-27 10:13:19 +0300

    feat(epub-reader): добавить переключение между страницами

--- src/features/epub-reader/EpubReader.tsx
+++ src/features/epub-reader/EpubReader.tsx
@@ -1,45 +1,85 @@
 import React from 'react';
 import styled from 'styled-components';
 import Epub, { Book, Rendition } from 'epubjs';
+import { ReaderProps } from 'shared/types';
 
-export interface EpubReaderProps {
-  url: string;
-}
+export interface EpubReaderProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    ReaderProps {}
 
-function EpubReader(props: EpubReaderProps) {
-  const { url } = props;
+const EpubReader = React.forwardRef<HTMLDivElement, EpubReaderProps>(
+  function EpubReader(props, ref) {
+    const { url, ...other } = props;
 
-  const bookRef = React.useRef<Book>();
-  const renditionRef = React.useRef<Rendition>();
-  const rootRef = React.useRef<HTMLDivElement>(null);
+    const bookRef = React.useRef<Book>();
+    const renditionRef = React.useRef<Rendition>();
+    const containerRef = React.useRef<HTMLDivElement>(null);
 
-  const initBook = React.useCallback(() => {
-    bookRef.current = Epub(url);
-  }, [url]);
+    const initBook = React.useCallback(() => {
+      bookRef.current = Epub(url);
+    }, [url]);
 
-  const initRender = React.useCallback(() => {
-    if (bookRef.current) {
-      renditionRef.current = bookRef.current.renderTo(rootRef.current as any, {
-        width: '100%',
-        height: '100%',
-      });
-      renditionRef.current.display();
-    }
-  }, []);
+    const initRender = React.useCallback(() => {
+      if (renditionRef.current) {
+        renditionRef.current.destroy();
+      }
 
-  React.useEffect(() => {
-    initBook();
-    initRender();
-  }, [initBook, initRender]);
+      if (bookRef.current && containerRef.current) {
+        renditionRef.current = bookRef.current.renderTo(containerRef.current, {
+          width: '100%',
+          height: '100%',
+        });
+        renditionRef.current.display();
+      }
+    }, []);
 
-  return <EpubReaderRoot ref={rootRef} />;
-}
+    React.useEffect(() => {
+      initBook();
+      initRender();
+    }, [initBook, initRender]);
+
+    const handlePrev = () => {
+      if (renditionRef.current) {
+        renditionRef.current.prev();
+      }
+    };
+
+    const handleNext = () => {
+      if (renditionRef.current) {
+        renditionRef.current.next();
+      }
+    };
+
+    return (
+      <EpubReaderRoot {...other} ref={ref}>
+        <EpubReaderActionBar>
+          <button onClick={handlePrev}>Prev</button>
+          <button onClick={handleNext} style={{ marginLeft: 10 }}>
+            Next
+          </button>
+        </EpubReaderActionBar>
+        <EpubReaderContainer ref={containerRef} />
+      </EpubReaderRoot>
+    );
+  },
+);
 
 const EpubReaderRoot = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 720px;
+  margin: 50px auto;
+`;
+
+const EpubReaderActionBar = styled.div`
+  display: flex;
+`;
+
+const EpubReaderContainer = styled.div`
+  flex: 0 0 auto;
+  width: 100%;
   height: 1024px;
   border: 1px solid #999999;
-  margin: 50px auto;
 `;
 
 export default EpubReader;
